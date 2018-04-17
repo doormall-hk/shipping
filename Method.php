e33f92340359afd9584a392502ac4a8d69e58bb1
Dmitry Fedyuk <dfediuk@gmail.com>
2018-04-17 14:20:28 +0600

    0.0.3

--- Method.php
+++ Method.php
@@ -1,9 +1,11 @@
 <?php
 namespace Doormall\Shipping;
-use Magento\Quote\Model\Quote\Address\RateRequest as R;
+use Magento\Quote\Model\Quote\Address\RateRequest as Req;
+use Magento\Quote\Model\Quote\Address\RateResult\Method as ResM;
 use Magento\Shipping\Model\Carrier\AbstractCarrier as AC; // 2018-04-17 It is used by PHPDoc.
 use Magento\Shipping\Model\Carrier\AbstractCarrierInterface as IAC; // 2018-04-17 It is used by PHPDoc.
 use Magento\Shipping\Model\Carrier\CarrierInterface as IC; // 2018-04-17 It is used by PHPDoc.
+use Magento\Shipping\Model\Rate\Result as Res;
 // 2018-04-17
 class Method extends \Df\Shipping\Method {
 	/**
@@ -11,10 +13,22 @@ class Method extends \Df\Shipping\Method {
 	 * @override
 	 * @see IAC::collectRates()
 	 * https://github.com/magento/magento2/blob/2.2.3/app/code/Magento/Shipping/Model/Carrier/AbstractCarrierInterface.php#L24-L31
-	 * @param R $r
-	 * @return mixed
+	 * @param Req $req
+	 * @return Res
 	 */
-	function collectRates(R $r) {return null;}
+	function collectRates(Req $req) {
+		$r = df_new_om(Res::class); /** @var Res $r */
+		$m = df_new_omd(ResM::class, [
+			'carrier' => $this->getCarrierCode()
+			,'carrier_title' => 'Pick-up'
+			,'cost' => 10
+			,'method' => 0
+			,'method_title' => 'Default'
+			,'price' => 10
+		]); /** @var ResM $m */
+		$r->append($m);
+		return $r;
+	}
 
 	/**
 	 * 2018-04-17
@@ -54,7 +68,13 @@ class Method extends \Df\Shipping\Method {
 	 *		}
 	 *	}
 	 * https://github.com/magento/magento2/blob/2.2.3/app/code/Magento/InstantPurchase/Model/ShippingMethodChoose/CarrierFinder.php#L41-L62
-	 * @return array
+	 * @return array(string => string)
 	 */
-	function getAllowedMethods() {return [];}
+	function getAllowedMethods() {return [0 => 'Default'];}
+
+	/**
+	 * 2018-04-17
+	 * @used-by \Df\Shipping\Method::codeS()
+	 */
+	const CODE = 'doormall';
 }
