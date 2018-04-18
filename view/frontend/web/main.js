01da9f4058023d97f33204544002595d8e53589a
Dmitry Fedyuk <dfediuk@gmail.com>
2018-04-19 02:51:23 +0600

    no message

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -5,4 +5,5 @@ define([
 /** 2017-09-06 @uses Class::extend() https://github.com/magento/magento2/blob/2.2.0-rc2.3/app/code/Magento/Ui/view/base/web/js/lib/core/class.js#L106-L140 */
 return parent.extend({
 	defaults: {template: 'Doormall_Shipping/main'}
+	,title: function() {return 'Hello, DoorMall!';}
 });});
