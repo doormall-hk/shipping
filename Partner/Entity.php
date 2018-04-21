f29a022c9993cdc29c4311875eab932aef1ea935
Dmitry Fedyuk <dfediuk@gmail.com>
2018-04-21 13:39:55 +0600

    no message

--- Partner/Entity.php
+++ Partner/Entity.php
@@ -3,10 +3,18 @@ namespace Doormall\Shipping\Partner;
 // 2018-04-19
 final class Entity extends \Df\Config\ArrayItem {
 	/**
+	 * 2018-04-21
+	 * @used-by \Doormall\Shipping\Method::collectRates()
+	 * @return int
+	 */
+	function fee() {return $this->f();}
+
+	/**
 	 * 2018-04-19
 	 * @override
 	 * @see \Df\Config\ArrayItem::id()
 	 * @used-by \Df\Config\A::get()
+	 * @used-by \Doormall\Shipping\Method::collectRates()
 	 * @return int
 	 */
 	function id() {return $this->v();}
@@ -19,6 +27,13 @@ final class Entity extends \Df\Config\ArrayItem {
 	 * @return int
 	 */
 	function sortWeight() {return 0;}
+
+	/**
+	 * 2018-04-21
+	 * @used-by \Doormall\Shipping\Method::collectRates()
+	 * @return int
+	 */
+	function title() {return $this->v(null, df_lang_zh(self::titleZH, self::titleEN));}
 
 	/** 2018-04-20 @used-by \Doormall\Shipping\Partner\FE::onFormInitialized() */
 	const data_url = 'data_url';
@@ -41,6 +56,17 @@ final class Entity extends \Df\Config\ArrayItem {
 	/** 2018-04-20 @used-by \Doormall\Shipping\Partner\FE::onFormInitialized() */
 	const id = 'id';
 
-	/** 2018-04-20 @used-by \Doormall\Shipping\Partner\FE::onFormInitialized() */
-	const title = 'title';
+	/**
+	 * 2018-04-21
+	 * @used-by title()
+	 * @used-by \Doormall\Shipping\Partner\FE::onFormInitialized()
+	 */
+	const titleEN = 'titleEN';
+
+	/**
+	 * 2018-04-21
+	 * @used-by title()
+	 * @used-by \Doormall\Shipping\Partner\FE::onFormInitialized()
+	 */
+	const titleZH = 'titleZH';
 }
