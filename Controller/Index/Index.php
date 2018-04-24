6ff3f73666f7351ae716108a2155e85cc0fba279
Dmitry Fedyuk <dfediuk@gmail.com>
2018-04-24 20:50:39 +0600

    no message

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -10,7 +10,7 @@ class Index extends _P {
 	 * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
 	 * @override
 	 * @see _P::execute()
-	 * @used-by \Magento\Framework\App\Action\Action::dispatch():
+	 * @used-by _P::dispatch():
 	 * 		$result = $this->execute();
 	 * https://github.com/magento/magento2/blob/2.2.1/lib/internal/Magento/Framework/App/Action/Action.php#L84-L125
 	 * @return Json
