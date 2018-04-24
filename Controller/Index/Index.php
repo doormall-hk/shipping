eba7cde8b4040a9006c8bab244a426acb27a05d9
Dmitry Fedyuk <dfediuk@gmail.com>
2018-04-24 20:49:45 +0600

    no message

---
+++ Controller/Index/Index.php
@@ -0,0 +1,21 @@
+<?php
+namespace Doormall\Shipping\Controller\Index;
+use Df\Framework\W\Result\Json;
+use Magento\Framework\App\Action\Action as _P;
+// 2018-04-24
+/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
+class Index extends _P {
+	/**
+	 * 2018-04-24
+	 * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
+	 * @override
+	 * @see _P::execute()
+	 * @used-by \Magento\Framework\App\Action\Action::dispatch():
+	 * 		$result = $this->execute();
+	 * https://github.com/magento/magento2/blob/2.2.1/lib/internal/Magento/Framework/App/Action/Action.php#L84-L125
+	 * @return Json
+	 */
+	function execute() {return Json::i([
+
+	]);}
+}
