45cc23d4462be54c0dde8f36f5ef4c3b0f78ccbf
Dmitry Fedyuk <dfediuk@gmail.com>
2018-04-30 17:52:57 +0600

    no message

--- Plugin/Checkout/Api/ShippingInformationManagementInterface.php
+++ Plugin/Checkout/Api/ShippingInformationManagementInterface.php
@@ -3,8 +3,6 @@ namespace Doormall\Shipping\Plugin\Checkout\Api;
 use Magento\Checkout\Api\Data\ShippingInformationInterface as ISI;
 use Magento\Checkout\Api\ShippingInformationManagementInterface as Sb;
 use Magento\Checkout\Model\ShippingInformation as SI;
-use Magento\Quote\Api\Data\CartInterface as IQ;
-use Magento\Quote\Model\Quote as Q;
 // 2018-04-30
 final class ShippingInformationManagementInterface {
 	/**
@@ -13,15 +11,14 @@ final class ShippingInformationManagementInterface {
 	 * https://github.com/magento/magento2/blob/2.2.3/app/code/Magento/Checkout/Api/ShippingInformationManagementInterface.php#L14-L22
 	 * @see \Magento\Checkout\Model\ShippingInformationManagement::saveAddressInformation():
 	 * https://github.com/magento/magento2/blob/2.2.3/app/code/Magento/Checkout/Model/ShippingInformationManagement.php#L140-L190
-	 * https://astrio.ru/blog/magento2-checkout-modifying
+	 * 1) https://astrio.ru/blog/magento2-checkout-modifying
+	 * 2) "How is a quote's `ext_shipping_info` property implemented and used?"
+	 * https://mage2.pro/t/5507
 	 * @param Sb $sb
 	 * @param int $qid
 	 * @param ISI|SI $si
 	 */
 	function beforeSaveAddressInformation(Sb $sb, $qid, ISI $si) {
-		$q = df_quote_r()->getActive($qid); /** @var IQ|Q $q */
-		// 2018-04-30 "How is a quote's `ext_shipping_info` property implemented and used?"
-		// https://mage2.pro/t/5507
-		$q->setExtShippingInfo($si->getExtensionAttributes()->getDoormallLocation());
+		df_quote_r()->getActive($qid)->setExtShippingInfo($si->getExtensionAttributes()->getDoormallLocation());
 	}
 }
